a491842a72346f11737bb0b6a83b74c9b8ad9fc7
Edvardas Rybakovas <edas1123@gmail.com>
2018-08-22 14:21:52 +0300

    finished cities list markup and css

--- src/app/module-city-list/city-list.component.ts
+++ src/app/module-city-list/city-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-city-list',
   templateUrl: './city-list.component.html',
-  styleUrls: ['./city-list.component.css']
+  styleUrls: ['./city-list.component.scss']
 })
 export class CityListComponent implements OnInit {
 
