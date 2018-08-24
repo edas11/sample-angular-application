5bf48e82ddb93930d4d33257d44324d16e0a5f85
Edvardas Rybakovas <edas1123@gmail.com>
2018-08-24 09:08:31 +0300

    deleted hard coded cities

--- src/app/module-city-list/city-list.component.ts
+++ src/app/module-city-list/city-list.component.ts
@@ -14,12 +14,6 @@ export class CityListComponent implements OnInit {
   constructor(private citiesDataSource: CitiesDataSourceService) { }
 
   ngOnInit() {
-    this.cities = [
-      new City('Vilnius', '2 hotels in total', 'assets/flags/lt-flag.png', 'assets/cities/Vilnius.jpg'),
-      new City('London', '3 hotels in total', 'assets/flags/gb-flag.png', 'assets/cities/London.jpg'),
-      new City('Paris', '3 hotels in total', 'assets/flags/fr-flag.png', 'assets/cities/Paris.jpg'),
-      new City('Berlin', '2 hotels in total', 'assets/flags/de-flag.png', 'assets/cities/Berlin.jpg')
-    ];
     this.cities = this.citiesDataSource.getCities();
   }
 
