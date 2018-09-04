7ba6d7b294656e811231f4750b83def9c8a6a1d4
Edvardas Rybakovas <edas1123@gmail.com>
2018-09-04 10:37:30 +0300

    deleted button component

--- src/app/module-widgets/widgets.module.ts
+++ src/app/module-widgets/widgets.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {OverlayPanelModule} from 'primeng/overlaypanel';
-import {SidebarModule} from 'primeng/sidebar';
 import {RatingModule} from 'primeng/rating';
 
 import { WRatingComponent } from './component-w-rating/w-rating.component';
 import { FormsModule } from '@angular/forms';
-import { WButtonComponent } from './component-w-button/w-button.component';
 
 @NgModule({
-  declarations: [WRatingComponent, WButtonComponent],
+  declarations: [WRatingComponent],
   imports: [
     CommonModule,
     FormsModule,
-    OverlayPanelModule,
-    SidebarModule,
     RatingModule
   ],
   exports: [
-    WRatingComponent,
-    WButtonComponent
+    WRatingComponent
   ]
 })
 export class WidgetsModule { }
